ee7b8672a2ec12c7d0fb45d98778010bfe51304e
Vitico99 <nenevictor99@gmail.com>
2021-12-07 03:48:28 +0100

    Add accesible cell definition and comparison utils for cells

--- src/board.pl
+++ src/board.pl
@@ -9,10 +9,9 @@
 :- dynamic currentColor/1. 
 :- dynamic availableBug/3. %availableBug(C, T, Cnt) there is Cnt bugs of type T and color C that can be placed
 
-% Utils
 :- dynamic visited/2. % visited(X,Y): cell(X,Y) has been visited by dfs.
 
-
+% adyacent/4
 % Adyacent definition for an hexagonal grid
 adyacent(X1,Y1,X2,Y2):- X2 is X1 - 1, Y2 is Y1.
 adyacent(X1,Y1,X2,Y2):- X2 is X1 -1 , Y2 is Y1 + 1.
@@ -21,45 +20,57 @@ adyacent(X1,Y1,X2,Y2):- X2 is X1, Y2 is Y1 + 1.
 adyacent(X1,Y1,X2,Y2):- X2 is X1 + 1, Y2 is Y1 - 1. 
 adyacent(X1,Y1,X2,Y2):- X2 is X1 + 1, Y2 is Y1.
 
-empty(X,Y):- \+ (bug(_,_,X,Y,_)). %ok
+% empty/2
+empty(X,Y):- \+ (bug(_,_,X,Y,_)). 
 
-emptyAdyacent(X1, Y1, X2, Y2):- %ok
+% emptyAdyacent/4
+emptyAdyacent(X1, Y1, X2, Y2):- 
     adyacent(X1, Y1, X2, Y2), 
     empty(X2, Y2).
 
-nonEmptyAdyacent(X,Y, X1, Y1):- %ok
-    adyacent(X,Y,X1,Y1), 
-    \+empty(X1,Y1).
+% nonEmptyAdyacent/4
+nonEmptyAdyacent(X1, Y1, X2, Y2):-
+    adyacent(X1, Y1, X2, Y2), 
+    \+empty(X2, Y2).
 
+% frontierAdyacent/4
 frontierAdyacent(X1, Y1, X2, Y2):-
     adyacent(X1, Y1, X2, Y2),
     frontier(X2, Y2).
 
-adyacentOpponent(X1, Y1, X2, Y2, C1):- %ok
+% adyacentOpponent/5
+adyacentOpponent(X1, Y1, X2, Y2, C1):-
     adyacent(X1, Y1, X2, Y2),
     bug(C2, _, X2, Y2, _),
     C1 \== C2.
 
-% nonIsolated(X1,Y1,X2,Y2):-
-%     nonEmptyAdyacent(X2, Y2, X3, Y3),
-%     X1 \== X3,
-%     Y1 \== Y3.
-
-% isolated(X1,Y1,X2,Y2):-
-%     \+ nonIsolated(X1,Y1,X2,Y2).
-
-
+% isIsolated/2
+% cell (X,Y) is isolated iff has only one non empty adyacent cell
 isIsolated(X,Y):-
     findall([X1,Y1], nonEmptyAdyacent(X,Y, X1,Y1),L),
     length(L, 1).
     
-isolatedEmptyAdyacent(X1, Y1, X2, Y2):- % X2, Y2 is only adyacnet to X1,Y1
+% isolatedEmptyAdyacent/4
+isolatedEmptyAdyacent(X1, Y1, X2, Y2):- % X2, Y2 is only adyacent to X1,Y1
     emptyAdyacent(X1,Y1,X2,Y2),
     isIsolated(X2,Y2).
 
-getAllPlaceableCells(PlaceablePositions):-
-    findall([X,Y],placeable(X,Y),PlaceablePositions).
+% accesibleCell/4
+accesibleCell(X1, Y1, X2, Y2):-
+    frontierAdyacent(X1,Y1,X2,Y2),
+    emptyAdyacent(X2,Y2, X3,Y3),
+    emptyAdyacent(X1, Y1, X3,Y3),    
+    nonEmptyAdyacent(X2,Y2, X4,Y4),
+    cellsAreDistinct(X1,Y1, X4,Y4).
 
+% cellNonStacked/2
+% there are no stacked bugs at cell (X,Y)
+cellNonStacked(X,Y):-
+    findall(S, bug(_,_,X,Y,S), Stack),
+    length(Stack, L),
+    L =< 1.
+
+% placeableByColor/3
 % Checks if a bug of Color C can be placed at cell (X,Y)
 placeableByColor(X,Y,C):- %ok
     frontier(X,Y), 
@@ -69,36 +80,32 @@ placeableByColor(X,Y,C):- % Edge case of the first bug of the second player
     firstBug(C),
     frontier(X,Y).
 
+
+% canBeRemoved/2
 % Checks if a bug at cell (X,Y) can be removed from the hive
 % A bug can be removed if and only if the hive remains connected without the bug.
-canBeRemoved(X,Y):-
+canBeRemoved(X, Y):-
     nonEmptyAdyacent(X,Y,X2,Y2), assertz(visited(X,Y)),
-    isBoardConnected(X2, Y2),
+    visit(X2, Y2),
     findall([X3, Y3], visited(X3,Y3), AllVisited),
     findall([X4, Y4], bug(_,_,X4,Y4,0), NonEmptyCells),!,
     (\+forall(visited(X1,Y1), retract(visited(X1,Y1)));
     same_length(AllVisited,NonEmptyCells)).
 
-isBoardConnected(X,Y):-
-    assertz(visited(X,Y)),
-    forall(toVisit(X,Y,X2,Y2), isBoardConnected(X2,Y2)).
+% visit/2
+visit(X, Y):-
+    assertz(visited(X, Y)),
+    forall(toVisit(X, Y,X2,Y2), visit(X2,Y2)).
 
-
-getBug(X,Y, S, bug(P,T,X,Y,S)):-%Get the bug in Position X,Y with stack number S
-    bug(P,T,X,Y,S).
-
-
+% toVisit/2
 toVisit(X,Y, X1,Y1):-
     nonEmptyAdyacent(X,Y,X1,Y1),
     \+visited(X1,Y1).
 
-
-
-
 % =============================== Board state modifying predicates
 
 initBoard(C1, C2):-
-    assertz(frontier(100,100)),
+    assertz(frontier(10,10)),
     assertz(color(C1)), assertz(color(C2)),
     assertz(firstBug(C2)),
     assertz(currentColor(C1)),
@@ -121,27 +128,57 @@ initBoard(C1, C2):-
     assertz(availableBug(C2, mosquito, 1)),
     assertz(availableBug(C2, pigbull, 1)).
 
+% changeCurrentColor/0
 changeCurrentColor:-
     %Add check if the non current player can move before updating
     color(C1), currentColor(C1),
     color(C2), \+ currentColor(C2),
     retract(currentColor(C1)), assertz(currentColor(C2)).
 
-placeBug(C,T,X,Y):- %ok
-    availableBug(C,T,Cnt), Cnt1 is Cnt - 1,
-    retract(availableBug(C,T,Cnt)), assertz(availableBug(C,T,Cnt1)),
+% placeBug/4
+placeBug(C,T,X,Y):- 
     checkFirstBug(C),
     assertz(bug(C,T,X,Y,0)), retractall(frontier(X,Y)), 
     forall(emptyAdyacent(X,Y,X1,Y1), assertz(frontier(X1, Y1))). % expand the frontier of the hive
 
+% removeBug/2
+removeBug(X,Y):- % Remove Position X,Y. Assumes there is only one bug in cell.
+    getBug(X,Y,0,Bug),
+    forall(isolatedEmptyAdyacent(X,Y,X1,Y1), retractall(frontier(X1,Y1))),
+    retract(Bug),
+    assertz(frontier(X,Y)).
+
+% updateBugCount/2
+updateBugCount(C,T):-
+    availableBug(C,T,Cnt1), Cnt2 is Cnt1 - 1,
+    retract(availableBug(C,T,Cnt1)), assertz(availableBug(C,T,Cnt2)).
+
+% checkFirstBug/1
 checkFirstBug(C):-
     \+ firstBug(C).
 
 checkFirstBug(C):-
     retract(firstBug(C)).
     
-removeBug(X,Y):- % Remove Position X,Y. Assumes there is only one bug in cell.
-    getBug(X,Y,0,Bug),
-    forall(isolatedEmptyAdyacent(X,Y,X1,Y1), retractall(frontier(X1,Y1))),
-    retract(Bug),
-    assertz(frontier(X,Y)).
+% ================================= Utills ==========================================    
+
+% cellsAreDistinct/4
+cellsAreDistinct(X1,_, X2,_):- X1\==X2, !.
+cellsAreDistinct(_, Y1,_, Y2):- Y1 \== Y2 ,!.
+ 
+% cellsAreDistinct/3
+cellsAreDistinct(_,_,[]).
+cellsAreDistinct(X1, Y1,[[X2,Y2]| R]):-
+    cellsAreDistinct(X1,Y1, X2, Y2),
+    cellsAreDistinct(X1,Y1, R).
+
+% cellsAreDistinct/1
+cellsAreDistinct([]).
+cellsAreDistinct([[X1,Y1]| R]):-
+    cellsAreDistinct(X1,Y1, R),
+    cellsAreDistinct(R).
+
+% getBug/4
+% Get the bug in Position X,Y with stack number S
+getBug(X, Y, S, bug(P,T,X,Y,S)):- 
+    bug(P,T,X,Y,S).
